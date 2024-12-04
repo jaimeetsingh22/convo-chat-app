9107766b4f6992bf97cd0ecd77bdee35892aeb75
jaimeetsingh22 <jaimeetsingh24@gmail.com>
2024-12-05 00:20:49 +0530

    updated the render image function

--- components/shared/RenderAttachment.jsx
+++ components/shared/RenderAttachment.jsx
@@ -1,6 +1,6 @@
 import { transformImage } from '@/utils/feature';
 import { FileOpen as FileOpenIcon } from '@mui/icons-material';
-import Image from 'next/image';
+
 import React from 'react'
 
 const RenderAttachment = (file, url) => {
@@ -9,7 +9,9 @@ const RenderAttachment = (file, url) => {
             return <video src={url} preload='none' width={'200px'} controls />
 
         case 'image':
-            return <Image src={transformImage(url, 200)} width={'200px'} height={'150px'} style={{
+            return <img src={transformImage(url, 200)} style={{
+                width: '200px',
+                height: '150px',
                 objectFit: 'contain',
             }} alt="Attachment" />
 
